2772d8e36ccb9c7714d62e2b54c9cd6d2b5cef6c
yubeen <yubeen@gbit.kr>
2025-04-18 23:46:16 +0900

    프롬프트 변경

--- common/time_utils.py
+++ common/time_utils.py
@@ -1,6 +1,6 @@
 import time
 import random
 
-def wait_random(min_sec=1.0, max_sec=3.0):
+def wait_random(min_sec=1.0, max_sec=2.0):
     delay = random.uniform(min_sec, max_sec)
     time.sleep(delay)
