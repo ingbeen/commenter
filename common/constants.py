d6aaa69131c9559626aed9779e195150a0c429c0
yubeen <yubeen@gbit.kr>
2025-05-11 23:30:21 +0900

    제외대상 추가

--- common/constants.py
+++ common/constants.py
@@ -1,5 +1,5 @@
 MY_BLOG_ID = "smy375"
-EXCLUDED_BLOG_IDS = {MY_BLOG_ID, "dadar", "doctor10", "sunsac05", "agmbattery1004", "momozi325", "swingandlife", "winner_shin", "onix81212", "nemohouse_911", "songsy9705", "eyelts", "onix81212", "lll1687", "wisdomsun06", "kk3830", "essecurity81"}
+EXCLUDED_BLOG_IDS = {MY_BLOG_ID, "dadar", "doctor10", "sunsac05", "agmbattery1004", "momozi325", "swingandlife", "winner_shin", "onix81212", "nemohouse_911", "songsy9705", "eyelts", "onix81212", "lll1687", "wisdomsun06", "kk3830", "essecurity81", "todaystories"}
 RECENT_COMMENTER_LIMIT = 200
 RECENT_POSTING_BUDDY_LIMIT = 500
 CAN_ADD_COMMENT_LIMIT = 100
