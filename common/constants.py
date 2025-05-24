2d123bf9603667f83844b357e987f6b34d7d0608
yblee <ingbeen@naver.com>
2025-05-24 14:59:22 +0900

    제외대상 추가

--- common/constants.py
+++ common/constants.py
@@ -1,7 +1,8 @@
 MY_BLOG_ID = "smy375"
 EXCLUDED_BLOG_IDS = {MY_BLOG_ID, "dadar", "doctor10", "sunsac05", "agmbattery1004", "momozi325", "swingandlife", "winner_shin", 
-                     "onix81212", "nemohouse_911", "songsy9705", "eyelts", "onix81212", "lll1687", "wisdomsun06", "kk3830", 
-                     "essecurity81", "todaystories" "onix81212", "ansgksrla", "ggui3", "hosanna1507", "ljh8257_", "jujigo", "llpf06669"}
+                     "onix81212", "nemohouse_911", "songsy9705", "eyelts", "lll1687", "wisdomsun06", "kk3830", "shgj2021" 
+                     "essecurity81", "todaystories", "ansgksrla", "ggui3", "hosanna1507", "ljh8257_", "jujigo", 
+                     "llpf06669", "minjun735", "dnwls1634", "oolleda", "gangzz3221", "topgun35", "login"}
 RECENT_COMMENTER_LIMIT = 200
 RECENT_POSTING_BUDDY_LIMIT = 500
 CAN_ADD_COMMENT_LIMIT = 100
